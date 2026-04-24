ead5e1b0268e5262018824f7edb3c5ef0771d353
harleyy1234 <harleyy@nycstudents.net>
2026-04-24 17:29:52 +0000

    when searching for recipes after the user clicks enter, the program displays loading recipes. I still need to figure out how to make it so the recipes actually show up.

--- index.js
+++ index.js
@@ -23,3 +23,52 @@ areaDropdown.appendChild(option);
 
 }
 
+
+async function searchMeals() {
+
+const query =
+document
+.getElementById("mealSearch")
+.value;
+
+const area =
+areaDropdown.value;
+
+results.innerHTML =
+"<p>Loading recipes...</p>";
+
+
+let url =
+"https://www.themealdb.com/api/json/v1/1/search.php?s=" + query;
+
+if(area){
+
+url =
+"https://www.themealdb.com/api/json/v1/1/filter.php?a=" + area;
+}
+
+
+const response =
+await fetch(url);
+
+const data =
+await response.json();
+
+results.innerHTML = "";
+
+if(!data.meals){
+
+results.innerHTML =
+"<p>No meals found</p>";
+
+return;
+}
+
+data.meals.forEach(meal => {
+
+displayMeal(meal.idMeal);
+
+});
+
+}
+
